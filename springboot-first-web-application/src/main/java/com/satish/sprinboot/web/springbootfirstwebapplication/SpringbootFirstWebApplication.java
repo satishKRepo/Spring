0aa07b9d58ff6eba8a474fb5cd8e240dd131f3a8
Satish Kesharwani <satishkumar.vitece@gmail.com>
2021-08-29 20:11:26 +0530

    read me updated

--- springboot-first-web-application/src/main/java/com/satish/sprinboot/web/springbootfirstwebapplication/SpringbootFirstWebApplication.java
+++ springboot-first-web-application/src/main/java/com/satish/sprinboot/web/springbootfirstwebapplication/SpringbootFirstWebApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootFirstWebApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootFirstWebApplication.class, args);
 	}
